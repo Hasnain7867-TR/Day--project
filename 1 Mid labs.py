db1a5bdf99fd47889633484997300f26b338e6cb
Hasnain7867-TR <www.mohammadhasnain786@gmail.com>
2025-07-22 12:35:09 +0530

    Create 1 Mid labs.py

---
+++ 1 Mid labs.py
@@ -0,0 +1,7 @@
+adjective1= input("enter the adjective1:")
+noun= input("enter the noun:")
+adjective2= input("enter the adjective2:")
+verb= input("enter the verb:")
+adjective3 = input("enter the adjective3:")
+
+print(f"Tomorrow i will go to {adjective1}club in Germany which is {noun} and people say after going there you will be {adjective2} and there is a pool where I will do {verb}ing and that pool is {adjective3} which is why i am nervous little bit")
